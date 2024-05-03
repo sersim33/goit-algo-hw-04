97c35fd30fcff185197606e8e53ecd8db277bfad
sersim33 <sersim33@gmail.com>
2024-05-03 14:12:55 +0300

    com2

---
+++ timsort.py
@@ -0,0 +1,11 @@
+import timeit
+import random
+
+arr2 = [random.randint(0, 100) for _ in range(50)]
+arr2.sort()  
+
+
+time_taken = timeit.timeit(lambda: arr2.sort(), number=100)
+
+print("Time taken for arr2.sort():", time_taken*1000, "seconds*1000")
+print(arr2)
